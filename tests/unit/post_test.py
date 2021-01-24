f46111b0481b74f821fb2ce29465ed29050ddce8
RedRubicon <brett@solarisit.co.za>
2021-01-24 17:20:43 +0200

    Update post_test.py: test_json(), test_repr()

--- tests/unit/post_test.py
+++ tests/unit/post_test.py
@@ -8,3 +8,24 @@ class PostTest(TestCase):
 
         self.assertEqual('Test', p.title)
         self.assertEqual('Test Content', p.content)
+        
+
+    def test_repr(self):
+        p = Post('Test', 'Test Content')
+
+        expected = "Test\n\nTest Content"
+        actual = repr(p)
+
+        self.assertEqual(actual, expected)
+
+
+    def test_json_returns_post_as_dictionary(self):
+        p = Post('Test', 'Test Content')
+
+        expected = {
+            'title': 'Test',
+            'content': 'Test Content',
+            }
+        actual = p.json()
+
+        self.assertDictEqual(actual, expected)
