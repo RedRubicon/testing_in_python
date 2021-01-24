6dfbd2a1c763bb54224dc94d4433d0a9549d39a3
RedRubicon <brett@solarisit.co.za>
2021-01-24 17:19:24 +0200

    Update post.py: create json(), override repr()
    create json() to return dict of post
    override repr()

--- post.py
+++ post.py
@@ -3,3 +3,12 @@ class Post:
         self.title = title
         self.content = content
 
+    def __repr__(self):
+        return f"{self.title}\n\n{self.content}"
+
+    def json(self):
+        return {
+            'title': self.title, 
+            'content': self.content,
+            }
+
